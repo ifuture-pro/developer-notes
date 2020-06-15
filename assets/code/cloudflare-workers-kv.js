4fa2c7c7ce1e646031dc965415434fba7beba9c3
ifuture-pro <strongrobot@163.com>
2020-06-15 22:48:17 +0800

    cloudflare workers

---
+++ assets/code/cloudflare-workers-kv.js
@@ -0,0 +1,121 @@
+/**
+cloudflare workers and workers-kv
+*/
+
+
+const html = todos => `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Todos</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet"></link>
+  </head>
+  <body class="bg-blue-100">
+    <div class="w-full h-full flex content-center justify-center mt-8">
+      <div class="bg-white shadow-md rounded px-8 pt-6 py-8 mb-4">
+        <h1 class="block text-grey-800 text-md font-bold mb-2">Todos</h1>
+        <div class="flex">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-800 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" placeholder="A new todo"></input>
+          <button class="bg-blue-500 hover:bg-blue-800 text-white font-bold ml-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline" id="create" type="submit">Create</button>
+        </div>
+        <div class="mt-4" id="todos"></div>
+      </div>
+    </div>
+  </body>
+  <script>
+    window.todos = ${todos}
+    var updateTodos = function() {
+      fetch("/", { method: 'PUT', body: JSON.stringify({ todos: window.todos }) })
+      populateTodos()
+    }
+    var completeTodo = function(evt) {
+      var checkbox = evt.target
+      var todoElement = checkbox.parentNode
+      var newTodoSet = [].concat(window.todos)
+      var todo = newTodoSet.find(t => t.id == todoElement.dataset.todo)
+      todo.completed = !todo.completed
+      window.todos = newTodoSet
+      updateTodos()
+    }
+    var populateTodos = function() {
+      var todoContainer = document.querySelector("#todos")
+      todoContainer.innerHTML = null
+      window.todos.forEach(todo => {
+        var el = document.createElement("div")
+        el.className = "border-t py-4"
+        el.dataset.todo = todo.id
+        var name = document.createElement("span")
+        name.className = todo.completed ? "line-through" : ""
+        name.textContent = todo.name
+        var checkbox = document.createElement("input")
+        checkbox.className = "mx-4"
+        checkbox.type = "checkbox"
+        checkbox.checked = todo.completed ? 1 : 0
+        checkbox.addEventListener('click', completeTodo)
+        el.appendChild(checkbox)
+        el.appendChild(name)
+        todoContainer.appendChild(el)
+      })
+    }
+    populateTodos()
+    var createTodo = function() {
+      var input = document.querySelector("input[name=name]")
+      if (input.value.length) {
+        window.todos = [].concat(todos, { id: window.todos.length + 1, name: input.value, completed: false })
+        input.value = ""
+        updateTodos()
+      }
+    }
+    document.querySelector("#create").addEventListener('click', createTodo)
+  </script>
+</html>
+`
+
+const defaultData = { todos: [] }
+
+const setCache = (key, data) => KRISTIAN_TODOS.put(key, data)
+const getCache = key => KRISTIAN_TODOS.get(key)
+
+async function getTodos(request) {
+  const ip = request.headers.get('CF-Connecting-IP')
+  const cacheKey = `data-${ip}`
+  let data
+  const cache = await getCache(cacheKey)
+  if (!cache) {
+    await setCache(cacheKey, JSON.stringify(defaultData))
+    data = defaultData
+  } else {
+    data = JSON.parse(cache)
+  }
+  const body = html(JSON.stringify(data.todos || []))
+  return new Response(body, {
+    headers: { 'Content-Type': 'text/html' },
+  })
+}
+
+async function updateTodos(request) {
+  const body = await request.text()
+  const ip = request.headers.get('CF-Connecting-IP')
+  const cacheKey = `data-${ip}`
+  try {
+    JSON.parse(body)
+    await setCache(cacheKey, body)
+    return new Response(body, { status: 200 })
+  } catch (err) {
+    return new Response(err, { status: 500 })
+  }
+}
+
+async function handleRequest(request) {
+  if (request.method === 'PUT') {
+    return updateTodos(request)
+  } else {
+    return getTodos(request)
+  }
+}
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
